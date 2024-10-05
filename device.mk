8020c45cd6f1a0e316b4d1e236b6b4a7e6dd7ddd
Drenzzz <naufalnazya@gmail.com>
2024-10-05 18:29:02 +0700

    garnet: overlay-orion: Add support OTA and Changelogs

--- device.mk
+++ device.mk
@@ -470,6 +470,10 @@ PRODUCT_PACKAGES += \
     WifiOverlayGarnetRedmiCN \
     WifiOverlayGarnetXIG05
 
+# Overlay-Orion
+DEVICE_PACKAGE_OVERLAYS += \
+    $(LOCAL_PATH)/overlay-orion
+
 # Power
 PRODUCT_PACKAGES += \
     android.hardware.power-service-qti
